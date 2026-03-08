42f0fda499a406a98401f35549d15423777629cf
alibattla-star <alibattla@gmail.com>
2026-03-08 19:09:17 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function videofunction (){
+document.getElementbyId("myComment").innerHTML += "A great vacation video";
+    }
+    function emailMessage(){
+        document.getElementById("emailBox").innerHTML += " Please enter a valid email address."
+    }
+    function blankMessage(){
+        document.getElementById("emailBox").innerHTML = "Email";
+    }
